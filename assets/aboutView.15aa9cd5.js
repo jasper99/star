a110a964a6d0a90a7fb602ff20bded2a392b2cad
jasper99 <38901684+jasper99@users.noreply.github.com>
2022-06-23 01:27:31 +0800

    Add files via upload

---
+++ assets/aboutView.15aa9cd5.js
@@ -0,0 +1,1 @@
+import{_ as t,o as a,c as o,p as c,e as s,a as _}from"./index.47ba3d0e.js";const d={},r=e=>(c("data-v-bc71aff8"),e=e(),s(),e),i={class:"row"},n=r(()=>_("div",{class:"col-12 title"},"\u95DC\u65BC\u6211\u5011",-1)),p=[n];function f(e,l){return a(),o("div",i,p)}var v=t(d,[["render",f],["__scopeId","data-v-bc71aff8"]]);export{v as default};
