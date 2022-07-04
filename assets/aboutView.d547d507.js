407acdc8a8e8a5b1f854bbacffea65fd93640be2
Jasper <cywang58@gmail.com>
2022-07-04 19:58:12 +0800

    bugfix & add 購物車頁面.表單驗證.

---
+++ assets/aboutView.d547d507.js
@@ -0,0 +1,1 @@
+import{_ as t,o as a,c as o,p as c,g as s,a as _}from"./index.0faeefa4.js";const d={},r=e=>(c("data-v-bc71aff8"),e=e(),s(),e),i={class:"row"},n=r(()=>_("div",{class:"col-12 title"},"\u95DC\u65BC\u6211\u5011",-1)),p=[n];function f(e,l){return a(),o("div",i,p)}var v=t(d,[["render",f],["__scopeId","data-v-bc71aff8"]]);export{v as default};
